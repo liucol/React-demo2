4ed74e81f54013488d0c99b4609dbec08f2372cf
liucol <1545766219@qq.com>
2017-09-26 11:44:54 +0800

    拷贝leancloud初始化代码

---
+++ src/TodoInput.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2017/9/25 0025.
+ */
+import React,{ Component } from 'react';
+import './TodoInput.css'
+export default class TodoInput extends  Component{
+    render(){
+        return <input className="TodoInput" type="text" value={this.props.content} onKeyPress={this.submit.bind(this)} onChange={this.changeTitle.bind(this)}/>
+    }
+    submit(e){
+        if(e.key==='Enter'){
+            this.props.onSubmit(e)
+        }
+    }
+    changeTitle(e){
+        this.props.onChange(e)
+    }
+}
